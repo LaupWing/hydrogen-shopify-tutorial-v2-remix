7e5df6616653e031727ee278720681140e646061
LaupWing <loc_strict@hotmail.com>
2023-02-21 09:17:57 +0100

    hhandle

--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -1,4 +1,22 @@
-const ProductHandle = () => {
-   return <div>ProductHandle</div>
+import { useLoaderData } from "@remix-run/react"
+import { json } from "react-router"
+
+export const loader = ({params}) => {
+   const { handle } = params
+
+   return json({
+      handle
+   })
 }
+
+const ProductHandle = () => {
+   const {handle} = useLoaderData()
+
+   return (
+      <div className="product-wrapper">
+         <h2>Product handle: {handle}</h2>
+      </div>
+   )
+}
+
 export default ProductHandle
