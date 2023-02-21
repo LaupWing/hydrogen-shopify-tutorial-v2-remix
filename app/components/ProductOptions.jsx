555c1f659ad3ce3abf865a2417d7498872d7213e
LaupWing <loc_strict@hotmail.com>
2023-02-21 09:55:47 +0100

    Product Options

---
+++ app/components/ProductOptions.jsx
@@ -0,0 +1,45 @@
+import { Link, useLocation } from "@remix-run/react"
+
+const ProductOptions = ({ options }) => {
+   const {pathname, search} = useLocation()
+
+   return (
+      <div className="grid gap-4 mb-6">
+         {options.map(option => {
+            if(!option.values.length){
+               return
+            }
+            return (
+               <div
+                  key={option.name}
+                  className="flex flex-col flex-wrap mb-4 gap-y-2 last:mb-0"
+               >
+                  <h3 className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-[4rem]">
+                     {option.name}
+                  </h3>
+
+                  <div className="flex flex-wrap items-baseline gap-4">
+                     {options.values.map((value) => {
+                        const linkParams = new URLSearchParams(search)
+                        linkParams.sest(option.name, value)
+
+                        return (
+                           <Link
+                              key={value}
+                              to={`${pathname}?${linkParams.toString()}`}
+                              preventScrollReset
+                              replace
+                              className="leading-none py-1 border-b-[1.5px] cursor-pointer transition-all duration-200"
+                           >
+                              {value}
+                           </Link>
+                        )
+                     })}
+                  </div>
+               </div>
+            )
+         })}
+      </div>
+   )
+}
+export default ProductOptions
