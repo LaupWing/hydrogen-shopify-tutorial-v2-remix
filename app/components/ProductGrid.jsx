6684bc0c785c933c8011c26105a9f6110c494327
LaupWing <loc_strict@hotmail.com>
2023-02-21 08:52:07 +0100

    Load more producsts

--- app/components/ProductGrid.jsx
+++ app/components/ProductGrid.jsx
@@ -1,6 +1,8 @@
 import ProductCard from "./ProductCard"
 
 const ProductGrid = ({ collection, url}) => {
+   const { products } = collection
+
    return (
       <section className="w-full gap-4 md:gap-8 grid">
          <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -8,6 +10,13 @@ const ProductGrid = ({ collection, url}) => {
                <ProductCard key={product.id} product={product}/>
             ))}
          </div>
+         {products.pageInfo.hasNextPage && (
+            <div className="flex items-center justify-center mt-6">
+               <button className="inline-block rounded font-medium text-center py-3 px-6 border w-full cursor-pointer">
+                  Load more products
+               </button>
+            </div>
+         )}
       </section>
    )
 }
