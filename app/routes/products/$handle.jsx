186a6fb275e91a6ff2a91d48e21460d75f0f646d
LaupWing <loc_strict@hotmail.com>
2023-02-21 09:24:45 +0100

    print product

--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -23,16 +23,26 @@ export const loader = async ({params, context}) => {
 }
 
 const ProductHandle = () => {
-   const {handle} = useLoaderData()
+   const {handle, product} = useLoaderData()
 
    return (
       <div className="product-wrapper">
          <h2>Product handle: {handle}</h2>
+         <PrintJson data={product}/>
       </div>
    )
 }
 
 export default ProductHandle
+
+const PrintJson = ({ data }) => {
+   return (
+      <details className="outline outline-2 outline-blue-300 py-4 my-2">
+         <summary>Product JSON</summary>
+         <pre>{JSON.stringify(data, null, 2)}</pre>
+      </details>
+   )
+}
 
 const PRODUCT_QUERY = `#graphql
    query product($handle: String!){
