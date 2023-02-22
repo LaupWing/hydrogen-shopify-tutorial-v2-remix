49d7a7b7a32178d1dfd30a003073d6793aff2f1c
LaupWing <loc_strict@hotmail.com>
2023-02-22 09:27:20 +0100

    Action

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,3 +1,19 @@
+
+export async function action({request, context}){
+   const {session, storefront} = context
+   const headers = new Headers()
+
+   const [formData, storedCartId, customerAccessToken] = await Promise.all([
+      request.formData(),
+      session.get("cartId"),
+      session.get("customerAccessToken")
+   ])
+   let cartId = storedCartId
+
+   let status = 200
+   let result
+}
+
 const Cart = () => {
    return <div>Cart</div>
 }
