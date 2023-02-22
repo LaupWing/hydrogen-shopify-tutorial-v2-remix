c80c7de3123209e34cfdd248171be7686ba55b80
LaupWing <loc_strict@hotmail.com>
2023-02-22 09:29:17 +0100

    Cart

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react"
 
 export async function action({request, context}){
    const {session, storefront} = context
@@ -15,6 +16,18 @@ export async function action({request, context}){
 }
 
 const Cart = () => {
-   return <div>Cart</div>
+   return (
+      <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+         <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+            Your cart is empty
+         </h2>
+         <Link
+            to={"/"}
+            className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+         >
+            Continue Shopping
+         </Link>
+      </div>
+   )
 }
 export default Cart
