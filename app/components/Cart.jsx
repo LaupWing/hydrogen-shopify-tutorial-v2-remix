59f3cab87d02d0ae1163f4f83e57d59c229dad89
LaupWing <loc_strict@hotmail.com>
2023-02-23 10:19:17 +0100

    LineItem

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,4 +1,5 @@
-import { flattenConnection } from "@shopify/hydrogen"
+import { Link } from "@remix-run/react"
+import { flattenConnection, Image, Money } from "@shopify/hydrogen"
 
 export const CartLineItems = ({ linesObj }) => {
    const lines = flattenConnection(linesObj)
@@ -12,3 +13,36 @@ export const CartLineItems = ({ linesObj }) => {
    )
 }
 
+const LineItem = ({ lineItem }) => {
+   const { merchandise, quantity } = lineItem
+
+   return (
+      <div className="flex gap-4">
+         <Link
+            to={`/products/${merchandise.product.handle}`}
+            className="flex-shrink-0"
+         >  
+            <Image 
+               data={merchandise.image}
+               width={110}
+               height={110}
+            />
+         </Link>        
+         <div className="flex-1">
+            <Link
+               to={`/products/${merchandise.product.handle}`}
+               className="no-underline hover:underline"
+            >
+               {merchandise.product.title}
+            </Link>
+            <div className="text-gray-800 text-sm">
+               {merchandise.title}
+            </div>
+            <div className="text-gray-800 text-sm">
+               Qty: {quantity}
+            </div>
+         </div>
+         <Money data={lineItem.cost.totalAmount}/>
+      </div>
+   )
+}
