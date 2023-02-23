bd46baa483c137b40ad48e16068d8d921da571a1
LaupWing <loc_strict@hotmail.com>
2023-02-23 10:29:39 +0100

    udpate

---
+++ app/queries/cart.js
@@ -0,0 +1,53 @@
+export const CART_QUERY = `#graphql
+   query CartQuery($cartId: ID!){
+      cart(id: $cartId) {
+         ...CartFragment
+      }
+   }
+   fragment CartFragment on Cart {
+      id
+      checkoutUrl
+      totalQuantity
+      buyerIdentity {
+         countryCode
+         customer {
+            id
+            email
+            firstName
+            lastName
+            displayName
+         }
+         email
+         phone
+      }
+      lines(first: 100){
+         edges {
+            node {
+               id
+               quantity
+               attributes {
+                  key
+                  value
+               }
+               cost {
+                  totalAmount {
+                     amount
+                     currencyCode
+                  }
+                  amountPerQuantity {
+                     amount
+                     currencyCode
+                  }
+                  compareAtAmountPerQuantity {
+                     amount
+                     currencyCode
+                  }
+               }
+               merchandise {
+                  
+               }
+            }
+         }
+      }
+   }
+`
