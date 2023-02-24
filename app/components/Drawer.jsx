0eef59afcf66df295343e568a9ae7f64e676f335
LaupWing <loc_strict@hotmail.com>
2023-02-24 08:36:19 +0100

    IconClose

--- app/components/Drawer.jsx
+++ app/components/Drawer.jsx
@@ -1,18 +1,10 @@
 import { Dialog, Transition } from "@headlessui/react"
 import { Fragment } from "react"
 
-export const Drawer = ({open, onClose, children}) => {
+export const Drawer = ({ open, onClose, children }) => {
    return (
-      <Transition 
-         appear 
-         show={open} 
-         as={Fragment}
-      >
-         <Dialog
-            as="div"
-            className={"relative z-50"}
-            onClose={onClose}
-         >
+      <Transition appear show={open} as={Fragment}>
+         <Dialog as="div" className={"relative z-50"} onClose={onClose}>
             <Transition.Child
                as={Fragment}
                enter="ease-out duration-300"
@@ -22,9 +14,37 @@ export const Drawer = ({open, onClose, children}) => {
                leaveFrom="opacity-100"
                leaveTo="opacity-0"
             >
-               <div className="fixed inset-0 bg-black bg-opacity-25"/>
+               <div className="fixed inset-0 bg-black bg-opacity-25" />
             </Transition.Child>
          </Dialog>
       </Transition>
+   )
+}
+
+function IconClose() {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 20 20"
+         className="w-5 h-5"
+      >
+         <title>Close</title>
+         <line
+            x1="4.44194"
+            y1="4.30806"
+            x2="15.7556"
+            y2="15.6218"
+            stroke="currentColor"
+            strokeWidth="1.25"
+         />
+         <line
+            y1="-0.625"
+            x2="16"
+            y2="-0.625"
+            transform="matrix(-0.707107 0.707107 0.707107 0.707107 16 4.75)"
+            stroke="currentColor"
+            strokeWidth="1.25"
+         />
+      </svg>
    )
 }
