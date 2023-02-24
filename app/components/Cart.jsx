59c4ef760ccb0e728e5a99d8fe6274db15b503d0
LaupWing <loc_strict@hotmail.com>
2023-02-24 08:19:48 +0100

    udpate

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -119,3 +119,20 @@ export function CartSummary({cost}) {
       </>
    )
 }
+
+export function CartActions({ checkoutUrl }){
+   if(!checkoutUrl){
+      return null
+   }
+
+   return (
+      <div className="flex flex-col mt-2">
+         <a 
+            href={checkoutUrl}
+            className="bg-black text-white px-6 py-3 w-full rounded-md text-center font-medium"
+         >
+            Continue to Checkout
+         </a>
+      </div>
+   )
+}
