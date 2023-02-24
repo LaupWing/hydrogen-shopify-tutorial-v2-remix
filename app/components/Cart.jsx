4e13467229a1a28a7796300ced90f6dfac41f5fc
LaupWing <loc_strict@hotmail.com>
2023-02-24 08:06:48 +0100

    IconRemove

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -7,12 +7,7 @@ export const CartLineItems = ({ linesObj }) => {
    return (
       <div className="space-y-8">
          {lines.map((line) => {
-            return (
-               <LineItem 
-                  key={line.id}
-                  lineItem={line}
-               />
-            )
+            return <LineItem key={line.id} lineItem={line} />
          })}
       </div>
    )
@@ -26,13 +21,9 @@ const LineItem = ({ lineItem }) => {
          <Link
             to={`/products/${merchandise.product.handle}`}
             className="flex-shrink-0"
-         >  
-            <Image 
-               data={merchandise.image}
-               width={110}
-               height={110}
-            />
-         </Link>        
+         >
+            <Image data={merchandise.image} width={110} height={110} />
+         </Link>
          <div className="flex-1">
             <Link
                to={`/products/${merchandise.product.handle}`}
@@ -40,14 +31,41 @@ const LineItem = ({ lineItem }) => {
             >
                {merchandise.product.title}
             </Link>
-            <div className="text-gray-800 text-sm">
-               {merchandise.title}
-            </div>
-            <div className="text-gray-800 text-sm">
-               Qty: {quantity}
-            </div>
+            <div className="text-gray-800 text-sm">{merchandise.title}</div>
+            <div className="text-gray-800 text-sm">Qty: {quantity}</div>
          </div>
-         <Money data={lineItem.cost.totalAmount}/>
+         <Money data={lineItem.cost.totalAmount} />
       </div>
+   )
+}
+
+function IconRemove() {
+   return (
+      <svg
+         fill="transparent"
+         stroke="currentColor"
+         viewBox="0 0 20 20"
+         className="w-5 h-5"
+      >
+         <title>Remove</title>
+         <path
+            d="M4 6H16"
+            strokeWidth="1.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+         <path d="M8.5 9V14" strokeLinecap="round" strokeLinejoin="round" />
+         <path d="M11.5 9V14" strokeLinecap="round" strokeLinejoin="round" />
+         <path
+            d="M5.5 6L6 17H14L14.5 6"
+            strokeWidth="1.25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+         <path
+            d="M8 6L8 5C8 4 8.75 3 10 3C11.25 3 12 4 12 5V6"
+            strokeWidth="1.25"
+         />
+      </svg>
    )
 }
