90f4afa2486ee5d5e62bf6a4dbf1894ebb31b884
LaupWing <loc_strict@hotmail.com>
2023-02-24 08:17:54 +0100

    Cartsummary

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -92,3 +92,30 @@ function IconRemove() {
       </svg>
    )
 }
+
+export function CartSummary({cost}) {
+   return (
+      <>
+         <dl className="space-y-2">
+            <div className="flex items-center justify-between">
+               <dt>Subtotal</dt>
+               <dd>
+                  {cost?.subtotalAmount?.amount ? (
+                     <Money data={cost?.subtotalAmount}/>
+                  ) : (
+                     "-"
+                  )}
+               </dd>
+            </div>
+            <div className="flex items-center justify-between">
+               <dt className="flex items-center">
+                  <span>Shipping estimate</span>
+               </dt>
+               <dd className="text-green-600">
+                  Free carbon neutral
+               </dd>
+            </div>
+         </dl>
+      </>
+   )
+}
