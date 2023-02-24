3b63b8553c5ec01ea7d2b3e5b423d22a09262c97
LaupWing <loc_strict@hotmail.com>
2023-02-24 08:49:36 +0100

    update

--- app/components/Drawer.jsx
+++ app/components/Drawer.jsx
@@ -16,6 +16,26 @@ export const Drawer = ({ open, onClose, children }) => {
             >
                <div className="fixed inset-0 bg-black bg-opacity-25" />
             </Transition.Child>
+
+            <div className="fixed inset-0">
+               <div className="absolute inset-0 overflow-hidden">
+                  <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
+                     <Transition.Child
+                        as={Fragment}
+                        enter="transform transition ease-in-out duration-500"
+                        enterFrom="translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transform transition ease-in-out duration-500"
+                        leaveFrom="translate-x-0"
+                        leaveTo="translate-x-full"
+                     >
+                        <Dialog.Panel className={"max-w-lg transform text-left align-middle shadow-xl"}>
+
+                        </Dialog.Panel>
+                     </Transition.Child>
+                  </div>
+               </div>
+            </div>
          </Dialog>
       </Transition>
    )
